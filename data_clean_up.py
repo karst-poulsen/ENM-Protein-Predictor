c996b71352a4dfb4d8732136fc04e2ca686dc452
Karsten Poulsen <poulsen.karsten@gmail.com>
2020-04-20 16:04:19 -0400

    uses a predict database and updated data_clean_up to output %aromaticity and hydrophobicity and such.

--- data_clean_up.py
+++ data_clean_up.py
@@ -1,30 +1,56 @@
 #from Bio  import SeqIO
 import csv
 import pandas as pd
+import numpy as np
 Data=[]
 AromaticAA = ['Y','W','F']
 PositiveAA=['R','H','K']
 NegativeAA=['D','E']
 HydrophobicAA=['A','C','L','I','M','F','Y','W']
-df = pd.read_csv('input2.csv')
+CysteineAA=['C']
+df = pd.read_csv('Chan_dataset_formatted.csv')
+dfout= pd.DataFrame(columns=['accession','pI','Protein Length','Protein Weight',
+                             'Enzyme Commision Number','Particle Size','Particle Charge',
+                             'Solvent Cysteine Conc','Solvent NaCl Conc','sequence','%Aromatic',
+                             '%Negative','%Postive','%Hydrophobic','%Cysteine','Protein Abundance',
+                             'Enrichment','Interprot'])
 Seq=df.Sequence
 Seq=Seq.astype(str)
+F=Seq.index.get_loc(Seq.last_valid_index())
+Seq=Seq[:800]
 print(Seq)
-df['Protein Length'] = len(Seq)
+Seq.fillna(0)
+print(Seq)
+#df['Protein Length'] = len(Seq)
 AromaticCount={}
-PercentAromatic={}
+PositiveCount={}
+NegativeCount={}
+HydrophobicCount={}
+CysteineCount={}
 for row in Seq:
     for aa in AromaticAA:
-        AromaticCount=Seq[row].count(aa)
-        print(AromaticCount)
+        AromaticCount[aa]=row.count(aa)
     for aa in PositiveAA:
-        PostiveCount=Seq[row].count(aa)
+        PositiveCount[aa]=row.count(aa)
     for aa in NegativeAA:
-        NegativeCount=Seq[row].count(aa)
+        NegativeCount[aa]=row.count(aa)
     for aa in HydrophobicAA:
-        HydrophobicCount=Seq[row].count(aa)
+        HydrophobicCount[aa]=row.count(aa)
+    for aa in CysteineAA:
+        CysteineCount[aa]=row.count(aa)
+    Length=len(row)
+    PercentAromatic=round(sum(list(AromaticCount.values()))*100/float(Length),3)
+    PercentPositive=round(sum(list(PositiveCount.values()))*100/float(Length),3)
+    PercentNegative=round(sum(list(NegativeCount.values()))*100/float(Length),3)
+    PercentHydrophobic=round(sum(list(HydrophobicCount.values()))*100/float(Length),3)
+    PercentCysteine=round(sum(list(CysteineCount.values()))*100/float(Length),3)
+    df2=pd.DataFrame([[Length,row,PercentAromatic,PercentPositive,PercentNegative,PercentHydrophobic,PercentCysteine]],
+                     columns=['Protein Length','Sequence','%Aromatic','%Postive','%Negative','%Hydrophobic','%Cysteine'])
+    dfout=pd.concat([dfout,df2])
+#print(dfout)
+dfout.to_csv('ProteinValues_Calculated.csv')
 
-    print(AromaticCount)
+    #print(PercentAromatic)
     #print(positiveCount)
     #print(NegativeCount)
     #Print(HydrophobicCount)
